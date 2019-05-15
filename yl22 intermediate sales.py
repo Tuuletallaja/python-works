186a1e333ceb8e87519f5223e41a00680eb7c838
Tuuletallaja <masugota@gmail.com>
2019-05-15 14:31:10 +0300

    update

--- yl22 intermediate sales.py
+++ yl22 intermediate sales.py
@@ -1,3 +1,8 @@
-revenue = {
-    
-}
+import csv
+
+with open('yl22_revenue.csv', 'r', newline='') as revenue:
+    revenue = csv.DictReader(revenue)
+    for lines in revenue:
+        print(lines)
+with open('yl22_expenses.csv', 'r', newline='') as expenses:
+    expenses = csv.DictReader(expenses)
